05b0355be59119a9896d7fdc821b37dc3fd8d87f
frank chukwu <frank chukwu>
2016-08-28 00:28:45 +0100

    create readline interface and get tweets of users

--- twitterFetch.js
+++ twitterFetch.js
@@ -11,4 +11,34 @@ var client = new twitter ({
   access_token_secret: process.env.access_token_secret,
 });
 
-	
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log("Hello there!");
+rl.question('Would you like to get tweets?  ', (answer) => {
+    if (answer == "Yes") {
+        rl.question('Please enter the USERID for tweets? ', (user)=> {
+
+            var options = {
+                screen_name: user,
+                count: 3
+            };
+			client.get('statuses/user_timeline', options, function(err, data, response) {
+				console.log(err, data, response.body);
+                for (var i = 0; i < data.length; i++) {
+                    var perc = i + 1;
+                    console.log('\nTweets ' + '----> ' + data[i].text + '\n');
+                }
+            });
+
+            rl.close();
+        });		
+	} else if (answer == "No") {        
+                console.log("Thank you for stopping by.")
+        
+    } else {
+        console.log('Please enter Yes or No to use this app');
+    }
+
+});		
